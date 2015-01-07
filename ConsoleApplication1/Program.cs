8cde062973ae8fbe052f63638c3156c563a95c85
Codevallis <codevallis@nym.hush.com>
2015-01-07 12:02:15 -0500

    Update 1

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+
+            Console.WriteLine(FeetToInches(30));
+            Console.WriteLine(FeetToInches(100));
+
+        }
+
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
         }
     }
 }
